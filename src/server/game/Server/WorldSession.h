4ca96f317b764a880404b7d0108210d61ec16358
unknown <espa@espa-b480710356.(none)>
2011-12-26 01:49:26 +0100

    Fix item ? ArkCore no tested

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -620,7 +620,7 @@ class WorldSession
         void HandleSwapInvItemOpcode(WorldPacket& recvPacket);
         void HandleDestroyItemOpcode(WorldPacket& recvPacket);
         void HandleAutoEquipItemOpcode(WorldPacket& recvPacket);
-        void HandleItemQuerySingleOpcode(WorldPacket& recvPacket);
+        void HandleRequestHotFix(WorldPacket& recvPacket);
         void HandleSellItemOpcode(WorldPacket& recvPacket);
         void HandleBuyItemInSlotOpcode(WorldPacket& recvPacket);
         void HandleBuyItemOpcode(WorldPacket& recvPacket);
